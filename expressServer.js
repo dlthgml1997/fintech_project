befef903a135632dec702c1fb31279067f19e685
dlthgml1997 <shfksthgml@naver.com>
2020-07-13 17:43:03 +0900

    add Project
    회원가입/ 로그인 / 토큰 발급 / 계좌 조회/ 본인 인증

--- expressServer.js
+++ expressServer.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const request = require("request");
+const jwt = require("jsonwebtoken");
+const auth = require("./lib/auth");
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password12#",
+  database: "fintech",
+  port: "3306",
+});
+
+connection.connect();
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
@@ -17,6 +30,156 @@ app.get("/", function (req, res) {
 app.get("/ejsTest", function (req, res) {
   res.render("test");
 });
+
+app.get("/designTest", function (req, res) {
+  res.render("design");
+});
+
+/** 2020.07.13 start */
+app.get("/signup", function (req, res) {
+  res.render("signup");
+});
+
+app.get("/authResult", function (req, res) {
+  var authCode = req.query.code;
+  console.log("사용자 인증코드 : ", authCode);
+  var option = {
+    method: "POST",
+    url: "https://testapi.openbanking.or.kr/oauth/2.0/token",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    //form 형태는 form / 쿼리스트링 형태는 qs / json 형태는 json ***
+    form: {
+      code: authCode,
+      //#자기 키로 시크릿 변경
+      client_id: "SM2eOU4s0ouUvvcvkIA5pBgfOVcF7VCzSs7qWKo7",
+      client_secret: "8rKpNTyQCJGeLNlGPULF2e0IAgU7Pem5wBfpeh0A",
+      redirect_uri: "http://localhost:3000/authResult",
+      grant_type: "authorization_code",
+    },
+  };
+  request(option, function (error, response, body) {
+    if (error) {
+      console.error(error);
+      throw error;
+    } else {
+      var accessRequestResult = JSON.parse(body);
+      console.log(accessRequestResult);
+      res.render("resultChild", { data: accessRequestResult });
+    }
+  });
+});
+
+app.post("/signup", function (req, res) {
+  console.log(req.body);
+  var userName = req.body.userName;
+  var userPassword = req.body.userPassword;
+  var userEmail = req.body.userEmail;
+  var userAccessToken = req.body.userAccessToken;
+  var userRefreshToken = req.body.userRefreshToken;
+  var userSeqNo = req.body.userSeqNo;
+
+  var sql =
+    "INSERT INTO user (`name`, `email`, `password`, `accesstoken`, `refreshtoken`, `userseqno`) VALUES (?, ?, ?, ?, ?, ?)";
+  connection.query(
+    sql,
+    [
+      userName,
+      userEmail,
+      userPassword,
+      userAccessToken,
+      userRefreshToken,
+      userSeqNo,
+    ],
+    function (error, results) {
+      if (error) throw error;
+      else {
+        res.json(1);
+      }
+    }
+  );
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.post("/login", function (req, res) {
+  console.log(req.body);
+  var userEmail = req.body.userEmail;
+  var userPassword = req.body.userPassword;
+  var sql = "SELECT * FROM user WHERE email = ?";
+  connection.query(sql, [userEmail], function (error, results) {
+    if (error) throw error;
+    else {
+      if (results.length == 0) {
+        res.json("등록되지 않은 회원입니다.");
+      } else {
+        var dbPassword = results[0].password;
+        console.log("db 에서 가져온 패스워드", dbPassword);
+        if (userPassword == dbPassword) {
+          var tokenKey = "f@i#n%tne#ckfhlafkd0102test!@#%";
+          jwt.sign(
+            {
+              userId: results[0].id,
+              userEmail: results[0].email,
+            },
+            tokenKey,
+            {
+              expiresIn: "10d",
+              issuer: "fintech.admin",
+              subject: "user.login.info",
+            },
+            function (err, token) {
+              console.log("로그인 성공", token);
+              res.json(token);
+            }
+          );
+        } else {
+          res.json("비밀번호가 다릅니다");
+        }
+      }
+    }
+  });
+});
+
+
+app.get("/authTest", auth, function (req, res) {
+  res.json("로그인이 완료된 사용자가 보는 화면");
+});
+
+
+app.get("/main", function (req, res) {
+  res.render("main");
+});
+/** 2020.07.13 end */
+
+/** 2020.07.13 homework */
+app.post("/list", function (req, res) {
+  //request 계좌 목록 조회 요청 만들기 request 모듈 활용
+  //res.json(aPI 결과 body 객체)
+  var option = {
+    method: "",
+    url: "",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    //form 형태는 form / 쿼리스트링 형태는 qs / json 형태는 json ***
+    form: {},
+  };
+  request(option, function (error, response, body) {
+    if (error) {
+      console.error(error);
+      throw error;
+    } else {
+      var resultJson = JSON.parse(body);
+      console.log(resultJson);
+      res.json(resultJson);
+    }
+  });
+});
+/** 2020.07.13 homework end */
 
 app.post("/ajaxTest", function (req, res) {
   var userId = req.body.sendUserId;
--- requestExample.js
+++ requestExample.js
@@ -2,9 +2,14 @@ const request = require("request");
 var parseString = require("xml2js").parseString;
 
 var url =
-  "http://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnld=109";
+//  'http://newsapi.org/v2/top-headlines?' +
+// 'country=us&' +
+// 'apiKey=78bc6ddd8cdb48ceac76f5f9b9dfc4c5';
+"http://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnld=109";
 request(url, function (error, response, body) {
-  //   console.log("body:", body); // Print the HTML for the Google homepage.
+    // console.log("body:", body.totalResults); // Print the HTML for the Google homepage.
+    // var jsObj = JSON.parse(body);
+    // console.dir(jsObj.articles[0].title)
   parseString(body, function (err, result) {
     console.dir(result.rss.channel[0].item[0].description[0].header[0].wf[0]);
   });
